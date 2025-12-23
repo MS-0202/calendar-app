b95b6d6e4f3c8a968b84293102a449720b05eb49
Mouri <isoft1060@gmail.com>
2025-12-23 19:21:49 +0900

    更新①

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,22 @@
 import app from "./firebase";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
+
+// ...firebaseConfigとappはそのまま...
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
+// デバイスによってポップアップかリダイレクトかを自動判定
+function isMobile() {
+  return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+}
+
 export function login() {
-  return signInWithPopup(auth, provider);
+  if (isMobile()) {
+    return signInWithRedirect(auth, provider);
+  } else {
+    return signInWithPopup(auth, provider);
+  }
 }
 
 export function logout() {
